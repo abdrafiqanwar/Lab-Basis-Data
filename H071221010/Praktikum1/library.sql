ec1dc56939f1093fc7f2f56717a7a0858e469cc1
tobiokagae <jovapertama@gmail.com>
2023-09-11 13:57:45 +0800

    praktikum1

---
+++ H071221010/Praktikum1/library.sql
@@ -0,0 +1,22 @@
+-- membuat database library
+CREATE DATABASE library;
+
+-- membuat table books beserta atribut di dalamnya
+CREATE TABLE books (
+id INT PRIMARY KEY,
+isbn VARCHAR(50) UNIQUE,
+title VARCHAR(50) NOT NULL,
+pages INT,
+summary TEXT,
+genre VARCHAR(50) NOT NULL);
+
+-- mengubah tipe data atribut isbn yang ada di table books
+ALTER TABLE books
+MODIFY isbn CHAR(13);
+
+-- menghapus atribut summary dari table books
+ALTER TABLE books
+DROP summary;
+
+-- menampilkan struktur table
+DESCRIBE books;
